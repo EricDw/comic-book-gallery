164457749af81da0606fdf688d849fe4d1178e1c
EricDw <dewildte@gmail.com>
2017-05-26 17:48:01 -0400

    Updated Comic Book Detail View to use the ViewBag.

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -12,6 +12,19 @@ namespace ComicBookGallery.Controllers
         {
             ActionResult _result;
 
+
+            ViewBag.SeriesTitle = "The Amazing Spiderman!";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p> Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
             _result = View();
 
             return _result;
